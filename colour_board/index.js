c8e02a379cb9d21fc08fc2fa5d231e3a5a36c3eb
B-gooroom <79742210+B-gooroom@users.noreply.github.com>
2021-07-24 12:57:37 +0900

    Add files via upload

---
+++ colour_board/index.js
@@ -0,0 +1,6 @@
+
+const modalToggle = function () {
+  document.body.classList.toggle('o-hidden');
+  document.getElementsByClassName('modal-background')[0].classList.toggle('active');
+  // if (modalToggle !== undefined) { }
+}
